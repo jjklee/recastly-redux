6d2b740b1858c5da003b7a64ebcdfc9203cfc43b
jjklee <jjlee720@gmail.com>
2019-06-12 12:34:34 -0700

    Implements actions

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,7 +1,19 @@
 import Redux from 'redux';
+import changeVideo from '../src/actions/currentVideo.js';
 
 var currentVideoReducer = (state, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
+  switch (action.type) {
+  case 'CHANGE_VIDEO':
+    return {
+      state.currentVideo: action.video
+    }
+    //change the state's video
+  default:
+    //change the state's video to null
+    return {
+      state.currentVideo: null;
+  }
 };
 
 export default currentVideoReducer;
--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -1,7 +1,8 @@
 import Redux from 'redux';
-
+// import cha
 var videoListReducer = (state = sampleData, action) => {
   //TODO: define a reducer for the videoList field of our state.
+  // switch (action.type) 
 };
 
 export default videoListReducer;
